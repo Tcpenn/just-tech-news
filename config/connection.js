17cde19ae11c436b1d3947c16d58dbff39cf8d4c
Tcpenn <tcpenn1026@gmail.com>
2021-11-20 21:44:03 -0500

    added upvoting functionality and vote model

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+//creates connection to db
 const sequelize = new Sequelize('just_tech_news_db', 'root', '264169Ty!', {
     host: 'localhost',
     dialect: 'mysql',
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+//imports models
 const User = require ('./User');
 const Post = require ('./Post');
+const Vote = require('./Vote');
 
 //creates associations
 User.hasMany(Post, {
@@ -11,4 +13,35 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+//many-to-many associations
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+
+//these routes connect the user and post models directly
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
+  
+//exports
+module.exports = { User, Post, Vote };
